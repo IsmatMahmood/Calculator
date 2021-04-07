a8fe84fe19210b9baa9d9ebedcbe75cf19f05029
Ismat Mahmood <ismatmahmood@hotmail.com>
2021-04-07 16:30:38 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Calculator!");
+            Console.WriteLine("Please enter your first number: ");
+            string firstNumber = Console.ReadLine();
+            Console.WriteLine("Please enter your second number: ");
+            string secondNumber = Console.ReadLine();
+
+            int answer = int.Parse(firstNumber) * int.Parse(secondNumber);
+            Console.WriteLine("Your answer is: " + answer);
+
+
+            Console.ReadLine();
+        }
+    }
+}
