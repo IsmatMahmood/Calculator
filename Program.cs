791a6107d568e60fc98c85c1f0b423cddf38ce52
Ismat Mahmood <ismatmahmood@hotmail.com>
2021-04-07 17:07:38 +0100

    Added multi operator function

--- Program.cs
+++ Program.cs
@@ -7,16 +7,62 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Calculator!");
+            string op = Op();
+            Console.WriteLine("Your valid operator is : " + op);
+            
+
             Console.WriteLine("Please enter your first number: ");
             string firstNumber = Console.ReadLine();
             Console.WriteLine("Please enter your second number: ");
             string secondNumber = Console.ReadLine();
 
-            int answer = int.Parse(firstNumber) * int.Parse(secondNumber);
+            int firstDigit = int.Parse(firstNumber);
+            int secondDigit = int.Parse(secondNumber);
+
+            int answer = Answer(op, firstDigit,secondDigit);
+            
             Console.WriteLine("Your answer is: " + answer);
 
 
             Console.ReadLine();
         }
+
+        private static int Answer(string op, int firstDigit, int secondDigit)
+        {
+            int answer = 0;
+            if (op == "+")
+            {
+                answer = firstDigit + secondDigit;
+            }
+            else if (op == "-")
+            {
+                answer = firstDigit - secondDigit;
+            }
+            else if (op == "/")
+            {
+                answer = firstDigit / secondDigit;
+            }
+            else if (op == "*")
+            {
+                answer = firstDigit * secondDigit;
+            }
+            
+            return answer;
+        }
+
+        private static string Op()
+        {
+            Console.WriteLine("Please enter an operator");
+            string val = Console.ReadLine();
+
+            while (val != "*" && val != "/" && val != "+" && val != "-")
+            {
+                Console.WriteLine("Please enter a valid operator");
+                val = Console.ReadLine();
+            }
+            return val;
+        }
+
+        
     }
 }
